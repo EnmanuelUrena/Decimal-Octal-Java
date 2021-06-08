585ba6d2b52b475f0bdb22a2164a193ff6b09a51
ENMANUEL URENA VARGAS <1095352@est.intec.edu.do>
2021-06-07 23:37:05 -0400

    Decimal-Octal v3

--- src/App.java
+++ src/App.java
@@ -1,23 +1,42 @@
-import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Type a number:");
-        int decimal = input.nextInt();
-        input.close();
-        System.out.println("Result: " + Decimal_Octal(decimal));
-    }
-    public static String Decimal_Octal(int decimal){
-        String octal = "";
-        String OctalCharacters = "01234567";
-        while(decimal > 0)
-        {
-            int residuo = decimal % 8;
-            octal = (OctalCharacters.charAt(residuo) + octal);
+        int DecNumber = 16;
+        boolean condition = true;
+        boolean condition2 = true;
+        int product = 0;
+        List<Integer> arrList = new ArrayList<Integer>(); 
+        var temp = DecNumber;
 
-            decimal /= 8;
+        while (condition) {
+            for (int i = 1; condition2 ; i++) {
+                product = i;
+                if (product * 8 == temp) {
+                    condition2 = false;
+                }
+                else if (product * 8 > temp) {
+                    product = product - 1;
+                    condition2 = false;
+                }
+            }
+            arrList.add(temp -(product*8));
+            temp = product;
+            if (temp == 1) {
+                arrList.add(temp);
+                condition = false;
+            }
+            else if (temp == 0) {
+                condition = false;
+            }
+            condition2 = true;
         }
-        return octal;
+
+        for (int i = arrList.size() -1; i >= 0; i--) {
+            System.out.print(arrList.get(i));
+        }
+
     }
+
 }
